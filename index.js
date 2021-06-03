0a4b112ec0ec6ea9ad0143ac5ca9756e9c36b6f9
Andrew Carrigan <carrigan223@gmail.com>
2021-06-03 14:15:37 -0700

    changed res data json

--- index.js
+++ index.js
@@ -42,9 +42,16 @@ server.post('/get-movie-details', (req, res) => {
                 dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
                 return res.json({
-                    speech: dataToSend,
-                    displayText: dataToSend,
-                    source: 'get-movie-details',
+                    fulfillmentMessages: [
+                        {
+                            text: {
+                                text: ['Text response from webhook'],
+                            },
+                        },
+                    ],
+                    // speech: dataToSend,
+                    // displayText: dataToSend,
+                    // source: 'get-movie-details',
                 });
             });
         },
